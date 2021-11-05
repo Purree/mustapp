c46d83e6230063dd5adb4bfdcc859b07246f6028
Pure <kirill2283371@mail.ru>
2021-11-05 19:23:31 +0300

    Add serials screen

--- src/components/main/screens/Notifications.js
+++ src/components/main/screens/Notifications.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {View} from 'react-native';
+import {Text} from "@ui-kitten/components";
 
 const Notifications = () => {
     return (
--- src/components/main/screens/Serials.js
+++ src/components/main/screens/Serials.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
+import {Divider, Layout, Text} from "@ui-kitten/components";
+import Header from "../../films/components/Header";
+import LargeFilmsPreview from "../../films/components/LargeFilmsPreview";
+import MediumFilmsPreview from "../../films/components/MediumFilmsPreview";
+import Collections from "../../films/components/Collections";
 
 const Serials = () => {
     return (
-        <>
-            <Text>Hello!</Text>
-        </>
+
+        <Layout>
+            <Header title='Сериалы'/>
+            <Divider/>
+            <View style={styles.container}>
+                <ScrollView>
+                    <MediumFilmsPreview title='Сериалы'/>
+                    <Collections />
+                </ScrollView>
+            </View>
+        </Layout>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        marginLeft: 15,
+        marginTop: 15,
+        marginBottom: 120
+    },
+    collectionsContainer: {
+        marginHorizontal: 15,
+        marginTop: 15,
+        marginBottom: 120
+    }
+})
 
 export default Serials;
